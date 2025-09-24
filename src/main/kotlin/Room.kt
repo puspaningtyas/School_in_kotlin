2c9a2319b39930a42fb06dfc2442a3149bbff32c
Neville Jeremy <nevillejro.laia@gmail.com>
2025-09-24 12:10:24 +0700

    feat: add method Room class

--- src/main/kotlin/Room.kt
+++ src/main/kotlin/Room.kt
@@ -1,8 +1,16 @@
 open class Room {
     open var tableList: List<Table> = listOf(Table())
-    open var chariList: List<Chair> = listOf(Chair())
+    open var chairList: List<Chair> = listOf(Chair())
     open var CSStaff: Staff = Staff()
 
     open fun addTable(table: Table) {}
     open fun addChair(chair: Chair) {}
+
+    open fun showTable(): List<Table> {
+        return tableList
+    }
+
+    open fun showChair(): List<Chair> {
+        return chairList
+    }
 }
