f6755bf7a57f3569edd5cffefbb5526a118c04b8
haneyasn <tyaasngrni@gmail.com>
2025-09-24 12:29:11 +0700

    Chair.kt

--- src/main/kotlin/Chair.kt
+++ src/main/kotlin/Chair.kt
@@ -1,7 +1,7 @@
 class Chair {
     val id: String = ""
-    val material: String,
-    var isOccupied: Boolean = false,
+    val material: String = ""
+    var isOccupied: Boolean = false
     var occupant: Person? = null
  }
 
