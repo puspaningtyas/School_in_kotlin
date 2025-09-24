0c34bfcd1cb2c1e7ba70e35e329a41a516ae61f2
Cristiano Kevin Pratama <Prtmakeviin@gmail.com>
2025-09-24 11:53:37 +0700

    tambah kelas room

---
+++ src/main/kotlin/Room.kt
@@ -0,0 +1,6 @@
+//class Room {
+//    val name: String,
+//    val csStaff: Staff,
+//    val tableList: MutableList<Table> = mutableListOf(),
+//    val chairList: MutableList<Chair> = mutableListOf()
+//}
