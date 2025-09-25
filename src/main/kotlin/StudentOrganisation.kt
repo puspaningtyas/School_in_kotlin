5669087d5d5ede9bee7764989486351b317b3d08
albettt <albertmichael221@gmail.com>
2025-09-25 19:28:44 +0700

    add: class StudentOrganisation.kt, atribut, and method

---
+++ src/main/kotlin/StudentOrganisation.kt
@@ -0,0 +1,20 @@
+data class Student(val studentName: String)
+
+class StudentOrganisation(
+    val name: String,
+    val advisor: String
+) {
+    private val leaders: MutableList<Student> = mutableListOf()
+
+    fun addLeader(student: Student) {
+        leaders.add(student)
+    }
+
+    fun removeLeader(student: Student) {
+        leaders.remove(student)
+    }
+
+    fun getLeaders(): List<Student> {
+        return leaders
+    }
+}
