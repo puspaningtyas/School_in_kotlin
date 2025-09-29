673e7ba32f0fbb3f97675909280e82c29047bb72
GabrielSigit <gabrielsigit360@gmail.com>
2025-09-29 19:06:48 +0700

    StudentOrga 1

---
+++ src/main/kotlin/StudentOrganisation.kt
@@ -0,0 +1,8 @@
+class StudentOrganisation {
+    val leaders: MutableList<Student> = mutableListOf()
+    )
+    {
+        fun addLeader(student: Student) {
+            leaders.add(student)
+        }
+    }
