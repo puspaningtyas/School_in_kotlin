4c6ccc7cdbe078bb4c0876b315c5fc183bf0a710
GabrielSigit <gabrielsigit360@gmail.com>
2025-09-30 19:05:40 +0700

    StudentOrga 1

--- src/main/kotlin/Student.kt
+++ src/main/kotlin/Student.kt
@@ -1,2 +1,11 @@
-class Student: Person() {
+class Student(
+    name: String,
+    age: Int,
+    val studentId: String,
+    val major: String
+) : Person(name, age) {
+
+    fun showInfo() {
+        println("Student: $name, Age: $age, ID: $studentId, Major: $major")
+    }
 }
