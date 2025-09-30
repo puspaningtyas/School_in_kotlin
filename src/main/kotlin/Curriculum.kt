f91694744fdbf36a6c3725fe837e2f3565818a99
PutriAnjelikha <putrianjelikha09@gmail.com>
2025-09-30 18:50:44 +0700

    commit

--- src/main/kotlin/Curriculum.kt
+++ src/main/kotlin/Curriculum.kt
@@ -1,2 +1,27 @@
 class Curriculum {
+//}
+// Definisi Subject
+class Subject(val name: String)
+
+    // Definisi Curriculum
+    class Curriculum(
+        var subjectList: MutableList<Subject> = mutableListOf()
+    ) {
+        // Tambah subject baru ke dalam kurikulum
+        fun addSubject(subject: Subject) {
+            subjectList.add(subject)
+        }
+
+        // Hapus subject dari kurikulum
+        fun removeSubject(subject: Subject) {
+            subjectList.remove(subject)
+        }
+
+        // Tampilkan semua subject dalam kurikulum
+        fun showSubjects() {
+            println("Daftar Subject dalam Curriculum:")
+            subjectList.forEach { println("- ${it.name}") }
+        }
+    }
+
 }
